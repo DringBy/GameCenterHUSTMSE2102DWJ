9aa70edae7fd706cc469ad1586e5f6945ac1251f
DringBy <lucky_skies@163.com>
2021-12-13 15:00:19 +0800

    Log updated. Committed the autorank v1.0. (12.13)

---
+++ include/autorank.h
@@ -0,0 +1,81 @@
+#pragma onec
+
+#include<string>
+#include<ctime>
+#include<vector>
+#include<fstream>
+#include <algorithm>
+#include <iostream>
+
+//得分数据块
+class scoreblock
+{
+friend class scoredealer;
+
+public:
+
+    //重载大于运算符 以分数为基准
+    bool operator > (const scoreblock& obj) const;
+    //重载小于运算符 以分数为基准
+    bool operator < (const scoreblock& obj) const;
+    //重载等于运算符 以分数为基准
+    bool operator == (const scoreblock& obj) const;
+
+    //一般构造函数
+    scoreblock(const std::string &Name, time_t PlayTime, unsigned long Score);
+    //默认构造函数
+    scoreblock();
+
+    //以字符串形式输出分数信息
+    const char* GetStrInf() const;
+
+private:
+
+    //玩家名称
+    std::string m_strName;
+    //计录时间
+    time_t m_ttPlayTime;
+    //玩家得分
+    unsigned long m_ulScore;
+
+    //记录分数信息的临时字符串
+    static char s_lpTmpStr[256];
+
+};
+
+//处理得分数据块
+class scoredealer
+{
+public:
+
+    //输出分数文件
+    //path :    输出文件地址
+    //返回值:   文件异常时返回false
+    static bool WriteFile(const char* path);
+    //写入分数文件
+    //path :    输出文件地址
+    //返回值:   文件异常时返回false
+    static bool ReadFile(const char* path);
+
+    //输出分数排名
+    //layer:    输出时各行预输出的制表符数
+    static void PrintList(int layer);
+    //插入分数块
+    static void Insert(const scoreblock& obj);
+
+private:
+
+    //隐藏构造函数
+    scoredealer();
+
+    //存储数据块的数组（向量）
+    static std::vector<scoreblock> s_vScoreList;
+    //被操作的文件
+    static std::fstream s_fFile;
+
+    //写入一个数据块
+    static void WriteData(const scoreblock&);
+    //读出一个数据块
+    static void ReadData(scoreblock&);
+
+};
---
+++ src/autorank.cpp
@@ -0,0 +1,185 @@
+#include "autorank.h"
+
+//初始化静态成员
+const int c_iTsSize = 256;
+char scoreblock::s_lpTmpStr[c_iTsSize] = {};
+
+std::vector<scoreblock> scoredealer::s_vScoreList;
+std::fstream scoredealer::s_fFile;
+
+bool scoreblock::operator > (const scoreblock& obj) const
+{
+    return m_ulScore > obj.m_ulScore;
+}
+
+bool scoreblock::operator < (const scoreblock& obj) const
+{
+    return m_ulScore < obj.m_ulScore;
+}
+
+bool scoreblock::operator == (const scoreblock& obj) const
+{
+    return m_ulScore == obj.m_ulScore;
+}
+
+scoreblock::scoreblock(const std::string& Name, time_t PlayTime, unsigned long Score):
+m_strName(Name),
+m_ttPlayTime(PlayTime),
+m_ulScore(Score)
+{}
+
+scoreblock::scoreblock():
+m_strName(""),
+m_ttPlayTime(0),
+m_ulScore(0)
+{}
+
+const char* scoreblock::GetStrInf() const
+{
+    //将时间转换为格式化的字符串
+    tm tmTimeStruct;
+    localtime_s(&tmTimeStruct, &m_ttPlayTime);
+
+    char lpTimeStr[20];
+    strftime(lpTimeStr, 20, "%Y-%m-%d %H:%M:%S", &tmTimeStruct);
+
+    //格式化输出
+    sprintf_s(s_lpTmpStr, c_iTsSize, "%-12u%s     %s\n", m_ulScore, lpTimeStr, m_strName.c_str());
+
+    return s_lpTmpStr;
+}
+
+scoredealer::scoredealer()
+{}
+
+void scoredealer::WriteData(const scoreblock& obj)
+{
+    //写入unsigned long型的二进制数据 后同
+    char* ptr = (char*)&obj.m_ulScore;
+
+    for(int i = 0; i < sizeof(unsigned long); i++)
+    {
+        s_fFile.put(ptr[i]);
+    }
+
+    ptr = (char*)&obj.m_ttPlayTime;
+
+    for(int i = 0; i < sizeof(time_t); i++)
+    {
+        s_fFile.put(ptr[i]);
+    }
+
+    s_fFile << obj.m_strName;
+    s_fFile.put('\0');
+}
+
+bool scoredealer::WriteFile(const char* path)
+{
+    bool is_suc = true;
+    s_fFile.open(path, std::ios::out | std::ios::binary);
+    size_t size = s_vScoreList.size();
+
+    if(s_fFile.good())
+    {
+        char* ptr = (char*)&size;
+        for(int i = 0; i < sizeof(size_t); i++)
+        {
+            s_fFile.put(ptr[i]);
+        }
+
+        //c11下的for迭代语法
+        for(auto i : s_vScoreList)
+        {
+            WriteData(i);
+        }
+    }
+    else
+    {
+        is_suc = false;
+    }
+
+    //关闭文件清除数据
+    s_vScoreList.clear();
+    s_fFile.close();
+    s_fFile.clear();
+
+    return is_suc;
+}
+
+void scoredealer::ReadData(scoreblock& obj)
+{
+    char ptr[sizeof(time_t)];
+
+    //读取unsigned long型的二进制数据 后同
+    s_fFile.read(ptr, sizeof(unsigned long));
+    obj.m_ulScore = *(unsigned long*)ptr;
+
+    s_fFile.read(ptr, sizeof(time_t));
+    obj.m_ttPlayTime = *(time_t*)ptr;
+
+    obj.m_strName.clear();
+    std::getline(s_fFile, obj.m_strName, '\0');
+}
+
+bool scoredealer::ReadFile(const char* path)
+{
+    bool is_suc = true;
+    char ptr[sizeof(time_t)];
+    s_fFile.open(path, std::ios::in | std::ios::binary);
+
+    s_fFile.read(ptr, sizeof(size_t));
+    size_t size = *(size_t*)ptr;
+
+    s_vScoreList.resize(size);
+
+    if(s_fFile.good())
+    {
+        for(int i = 0; i < size && s_fFile.good(); i++)
+        {
+            ReadData(s_vScoreList[i]);
+        }
+    }
+    else
+    {
+        is_suc = false;
+    }
+
+    //关闭文件
+    s_fFile.close();
+    s_fFile.clear();
+
+    return is_suc;
+}
+
+void scoredealer::PrintList(int layer)
+{
+    std::sort(s_vScoreList.rbegin(), s_vScoreList.rend());
+    
+    //lpLayerStr储存预放置的制表符
+    char* lpLayerStr = new char[layer + 1];
+    for(int i = 0; i < layer; i++)
+    {
+        lpLayerStr[i] = '\t';
+    }
+    lpLayerStr[layer] = '\0';
+
+    std::cout << lpLayerStr << "RANK:     SCORE:      PLAYTIME:               NAME:" << std::endl;
+
+    int rank = 1;
+    std::cout.setf(std::ios_base::left, std::ios_base::adjustfield);
+    for(scoreblock i : s_vScoreList)
+    {
+        std::cout << lpLayerStr;
+        std::cout.width(10);
+        std::cout << rank << i.GetStrInf() << std::endl;
+
+        rank++;
+    }
+
+    delete[] lpLayerStr;
+}
+
+void scoredealer::Insert(const scoreblock& obj)
+{
+    s_vScoreList.push_back(obj);
+}
